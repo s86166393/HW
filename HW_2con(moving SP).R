0d070cefb9e6128d0643ec20175abddf7580bbbb
hy song <72924309+shy86@users.noreply.github.com>
2021-01-19 13:35:04 +0800

    Update HW_2con(moving SP).R

--- HW_2con(moving SP).R
+++ HW_2con(moving SP).R
@@ -14,7 +14,7 @@ time <- ncvar_get(ncfile, "time")
 date <- time + ymd(ncfile[["dim"]][["time"]][["units"]])
 nc_close(ncfile)
 
-contpass_SPL <- function(data, prob, k = 1) {
+contpass_movingSPL <- function(data, prob, k = 1) {
   if (all(is.na(data)) == TRUE) {
     data[is.na(data)] <- FALSE
     return(data)
